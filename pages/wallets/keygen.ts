c3a8420c5255fdac9159e0c28848346e87dab2d1
sillyhousehodler <sillyhouse.xyz@gmail.com>
2024-04-07 12:08:50 +0800

    empty and modularize unused scripts

--- pages/wallets/keygen.ts
+++ pages/wallets/keygen.ts
@@ -1,15 +1,16 @@
-import * as fs from 'fs';
-import * as crypto from 'crypto';
+export {};
+// import * as fs from 'fs';
+// import * as crypto from 'crypto';
 
-const iv = crypto.randomBytes(16);
-const key = crypto.randomBytes(32);
-// let convertedNums = iv.toString('hex');
+// const iv = crypto.randomBytes(16);
+// const key = crypto.randomBytes(32);
+// // let convertedNums = iv.toString('hex');
 
-let jsonData = {
-    'iv': iv,
-    'key': key
-};
+// let jsonData = {
+//     'iv': iv,
+//     'key': key
+// };
 
-fs.writeFile("encryptedKey.json", JSON.stringify(jsonData), function(err){
-    if (err) console.log('error', err);
-})
+// fs.writeFile("encryptedKey.json", JSON.stringify(jsonData), function(err){
+//     if (err) console.log('error', err);
+// })
